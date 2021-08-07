be70399f8041df6dbe84588bcc24c7214c5f8685
alexpalapine2003 <37636391+alexpalapine2003@users.noreply.github.com>
2021-08-07 12:12:16 -0300

    Update BaseNumber.js

--- BaseNumber.js
+++ BaseNumber.js
@@ -4,29 +4,31 @@ const Normalize = s => {
   s.indexOf(".") + 1 == s.length && (s = s.substring(0, s.length - 1));
   return s;
 }
+const checkIncompatible = (n, base) => {
+  let digits = n.split("");
+  if (n === "" || digits.reduce((a, d) => d == "." ? ++a : a, 0) > 1 || 
+      (digits.some(d => isNaN(d) && (getNumber(d) < 10 || getNumber(d) > 35) && d != ".")))   return "Invalid number";
+  if (digits.some(d => getNumber(d) >= base  && d != "."))    return "Number doesnÂ´t match base";
+  if (isNaN(base))	return "Target base is Not A Number"; 
+  if (base < 2 || base > 36)  return "Invalid base argument";
+  return false;
+};
 
 class BaseNumber {
-    constructor(n, base) {
-  	const checkIncompatible = () => {
-      if (n === "" || digits.reduce((a, d) => d == "." ? ++a : a, 0) > 1 || 
-          (digits.some(d => isNaN(d) && (getNumber(d) < 10 || getNumber(d) > 35) && d != ".")))   return "Invalid number";
-      if (digits.some(d => getNumber(d) >= base  && d != "."))    return "Number doesn´t match base";
-      if (base < 2 || base > 36)  return "Invalid base argument";
-      return false;
-    };
-    
+	#number; #base; #isFloat;
+  constructor(n, base = 10) {  
     n = Normalize(n);
     let digits = n.split("");
     base = parseInt(base);
-    let state = checkIncompatible();
-    if (state) throw "Error creating number: " + state; 
+    const state = checkIncompatible(n, base);
+    if (state) throw "error building number: " + state; 
     
-    this.number = n;
-    this.base = base;
-    this.isFloat = !(digits.reduce((a, d) => d == "." ? ++a : a, 0) < 1);
+    this.#number = n;
+    this.#base = base;
+    this.#isFloat = !(digits.reduce((a, d) => d == "." ? ++a : a, 0) < 1);
   }
   
-  fixed(precision) {
+  fixed(precision, exclusive = false) {
     const add = p => {
       if (digits[p] == ".") add(p - 1);
       else if (getNumber(digits[p]) + 1 == base) {
@@ -38,71 +40,109 @@ class BaseNumber {
       }
     };
 
-    const base = this.base;
-    let n = this.number;
+    const base = this.#base;
+    let n = this.#number;
     let digits = n.split("");
-    if (precision < 0) throw "Error rounding number: Invalid precision argument"; 
+    precision = parseInt(precision);
+    if (precision < 0 || isNaN(precision)) throw "error rounding number: Invalid precision argument"; 
     
     if (typeof digits[digits.indexOf(".") + 1 + precision] === "undefined" ||
-        !this.isFloat)   return n;
+        !this.#isFloat)   return n;
 
-    base - getNumber(digits[digits.indexOf(".") + 1 + precision]) <= base / 2 
+    (base - !!exclusive) - getNumber(digits[digits.indexOf(".") + 1 + precision]) <= (base - !!exclusive) / 2 
     && add(digits.indexOf(".") + precision);
     digits.splice(digits.indexOf(".") + 1 + precision);
     !precision && digits.splice(-1);
     return digits.join("");
   }
   
-  higherThan(target, base = target.base) {
-    const checkIncompatible = () => {
-      if (n === "" || digits.reduce((a, d) => d == "." ? ++a : a, 0) > 1 || 
-          (digits.some(d => isNaN(d) && (getNumber(d) < 10 || getNumber(d) > 35) && d != ".")))   return "Invalid target number";
-      if (digits.some(d => getNumber(d) >= base  && d != "."))    return "Target number doesn´t match base";
-      if (typeof base === "undefined")	return "Target base is undefined"; 
-      if (base < 2 || base > 36)  return "Invalid target base argument";
-      return false;
-    };
-   
-    let n = Normalize(typeof target.number !== "undefined" ? target.number : target);
-    let digits = n.split("");
-    let state = checkIncompatible();
-    if (state) throw "Error comparing number: " + state; 
-    if (parseInt(this.number.split(".")[0], this.base) === parseInt(n.split(".")[0], base)){
-    	const thisVal = this.isFloat ? this.number.split(".")[1].split("").reduce((a, d, i) => a + d * Math.pow(this.base, -(i + 1)), 0) : 0;
-      const targetVal = typeof n.split(".")[1] !== "undefined" ? n.split(".")[1].split("").reduce((a, d, i) => a + d * Math.pow(base, -(i + 1)), 0) : 0;
-      if (thisVal > targetVal) return true;
-      else return false;
-    }else if (parseInt(this.number.split(".")[0], this.base) > parseInt(n.split(".")[0], base))	
-    	return true;
-    else return false;
+  higherThan(target, base = 10) {
+    const targetVal = parseFloat(target instanceof BaseNumber ? target.parseBase() : (new BaseNumber(target, base)));
+    return parseFloat(this.parseBase()) > targetVal;
   }
   
-  lowerThan(target, base = target.base) {
-    const checkIncompatible = () => {
-      if (n === "" || digits.reduce((a, d) => d == "." ? ++a : a, 0) > 1 || 
-          (digits.some(d => isNaN(d) && (getNumber(d) < 10 || getNumber(d) > 35) && d != ".")))   return "Invalid target number";
-      if (digits.some(d => getNumber(d) >= base  && d != "."))    return "Target number doesn´t match base";
-      if (typeof base === "undefined")	return "Target base is undefined"; 
-      if (base < 2 || base > 36)  return "Invalid target base argument";
-      return false;
-    };
-   
-    let n = Normalize(typeof target.number !== "undefined" ? target.number : target);
-    let digits = n.split("");
-    let state = checkIncompatible();
-    if (state) throw "Error comparing number: " + state; 
-    if (parseInt(this.number.split(".")[0], this.base) === parseInt(n.split(".")[0], base)){
-    	const thisVal = this.isFloat ? this.number.split(".")[1].split("").reduce((a, d, i) => a + d * Math.pow(this.base, -(i + 1)), 0) : 0;
-      const targetVal = typeof n.split(".")[1] !== "undefined" ? n.split(".")[1].split("").reduce((a, d, i) => a + d * Math.pow(base, -(i + 1)), 0) : 0;
-      if (thisVal < targetVal) return true;
-      else return false;
-    }else if (parseInt(this.number.split(".")[0], this.base) < parseInt(n.split(".")[0], base))	
-    	return true;
-    else return false;
+  lowerThan(target, base = 10) {
+    const targetVal = parseFloat(target instanceof BaseNumber ? target.parseBase() : (new BaseNumber(target, base)));
+    return parseFloat(this.parseBase()) < targetVal;
   }
   
-  equalTo(target, base = target.base) {
-  	if (!this.higherThan(target, base) && !this.lowerThan(target, base))	return true;
-    return false;
+  equalTo(target, base = 10) {
+  	const targetVal = parseFloat(target instanceof BaseNumber ? target.parseBase() : (new BaseNumber(target, base)));
+    return parseFloat(this.parseBase()) === targetVal;
   }
+  
+  parseBase(base = 10) {
+    base = parseInt(base);
+    const state = checkIncompatible("0", base);
+    if (state) throw "error parsing number: " + state; 
+  	const int = parseInt(this.#isFloat ? this.#number.split(".")[0] : this.#number, this.#base);
+    let dec = this.#isFloat ? this.#number.split(".")[1].split("").reduce((a, d, i) => a + getNumber(d) * Math.pow(this.#base, -(i + 1)), 0) : 0;
+    
+    const parsedInt = int.toString(base);
+    let timeout = 100, parsedDec = [];
+    while (timeout && (dec - Math.floor(dec))) {
+      dec = parseFloat(((dec - Math.floor(dec)) * base).toFixed(15));
+      const toPush = Math.floor(dec);
+      parsedDec.push(toPush > 9 ? String.fromCharCode(toPush + 87) : toPush);
+    	timeout--;
+    }
+    return parsedInt + (parsedDec.length ? "." + parsedDec.join("") : "");
+  }
+  
+  newValue(n, base = this.#base) {
+    const temp = this.#number;
+    if (n instanceof BaseNumber) {
+    	Object.assign(this, n);
+    }else{
+      n = Normalize(n);
+      base = parseInt(base);
+      const state = checkIncompatible(n, base); 
+      if (state) throw "error modifying number: " + state; 
+      this.#number = n;
+      this.#base = base;
+      this.#isFloat = !(n.split("").reduce((a, d) => d == "." ? ++a : a, 0) < 1);
+    }
+    return temp;
+  }
+  
+  newBase(base) {
+  	base = parseInt(base);
+    const state = checkIncompatible("0", base);
+    if (state) throw "error parsing number: " + state; 
+    this.#number = this.parseBase(base);
+    this.#base = base;
+    return this.#number;
+  }
+  
+  add(target, base = 10, resultBase = this.#base) {
+    return target instanceof BaseNumber ?
+    new BaseNumber(parseFloat(this.parseBase()) + parseFloat(target.parseBase())).parseBase(resultBase) :
+    new BaseNumber(parseFloat(this.parseBase()) + parseFloat(new BaseNumber(target, base).parseBase())).parseBase(resultBase);
+  }
+  
+  subtract(target, base = 10, resultBase = this.#base) {
+  	return target instanceof BaseNumber ?
+    new BaseNumber(parseFloat(this.parseBase()) - parseFloat(target.parseBase())).parseBase(resultBase) :
+    new BaseNumber(parseFloat(this.parseBase()) - parseFloat(new BaseNumber(target, base).parseBase())).parseBase(resultBase);
+  }
+  
+  multiply(target, base = 10, resultBase = this.#base) {
+  	return target instanceof BaseNumber ?
+    new BaseNumber(parseFloat(this.parseBase()) * parseFloat(target.parseBase())).parseBase(resultBase) :
+    new BaseNumber(parseFloat(this.parseBase()) * parseFloat(new BaseNumber(target, base).parseBase())).parseBase(resultBase);
+  }
+  
+  divide(target, base = 10, resultBase = this.#base) {
+  	return target instanceof BaseNumber ?
+    new BaseNumber(parseFloat(this.parseBase()) / parseFloat(target.parseBase())).parseBase(resultBase) :
+    new BaseNumber(parseFloat(this.parseBase()) / parseFloat(new BaseNumber(target, base).parseBase())).parseBase(resultBase);
+  }
+  
+  toDec() {	return this.parseBase();	}
+  toHex() {	return this.parseBase(16);}
+  toBin() {	return this.parseBase(2);	}
+  toOct() {	return this.parseBase(8);	}
+  
+  value() { return this.#number;	}
+  base() { return this.#base;	}
 }

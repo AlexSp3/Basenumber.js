2083b65404e57f26f33dae87994298f4c6e0969d
alexpalapine2003 <37636391+alexpalapine2003@users.noreply.github.com>
2021-08-10 11:50:51 -0300

    Create hexadecimal-addition.js

---
+++ src/examples/hexadecimal-addition.js
@@ -0,0 +1,16 @@
+/* 
+ * @Example: add two hexadecimal numbers and transform the result in decimal base
+ * 
+ * @Author: Alexandro Palacios 
+ * @Last edit: 10/08/2021
+ */
+
+const num1 = prompt("Write first hexadecimal number");
+
+const num2 = prompt("Write second hexadecimal number");
+
+const hex = new BaseNumber(num1, 16);
+
+const result = new BaseNumber(  hex.add(num2, 16, 10) );   // base 10 number
+
+console.log(result.value());
